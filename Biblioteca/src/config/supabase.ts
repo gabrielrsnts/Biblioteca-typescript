40702c595348aec64abbb84b4f3d12fd6f6d5d48
Gabriel Rodrigues Santos <gabrielrodriguesec63@gmail.com>
2025-09-19 13:58:09 -0300

    Update supabase.ts

--- Biblioteca/src/config/supabase.ts
+++ Biblioteca/src/config/supabase.ts
@@ -4,8 +4,6 @@ import dotenv from 'dotenv';
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://fsnwrgskggwopdauhggs.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzbndyZ3NrZ2d3b3BkYXVoZ2dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5NzEwMzgsImV4cCI6MjA2NDU0NzAzOH0.OfvLottXr5O-8gy7DHybhCyhVDNg44za7bNAUywVtLw';
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('As credenciais do Supabase não foram configuradas corretamente');
